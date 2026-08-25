94e69ba21dc904be5fbd08e99ecd7dc982a09143
gunda6566-dot <gunda6566@gmail.com>
2026-08-25 23:12:19 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,1 @@
-const CACHE='stenopro-premium-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='stenopro-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
